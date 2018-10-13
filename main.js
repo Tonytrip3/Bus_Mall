461067bbe916a4a400d82f6c0bb961e7f53b12b7
Anthony Triplett <anthony.triplett1989@gmail.com>
2018-10-13 10:54:22 -0700

    added all necessary files

---
+++ main.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var busmallContainer = document.getElementById('busmall');
+
+var Image = function(values){
+  //builds images
+};
+
+var allImages = [];
+
+var chooseNewImage(){
+
+};
+
+var handleBusmallClick = function(event){
+//check to make sure we click on the correct image
+
+//increment the correct image's likes
+
+//increment all current image appeared count
+
+//call choose new
+};
+
+busmallContainer.addEventListener('click', handleBusmallClick);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+//Charts
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+//function to render the chart
+var renderChart = function(){
+//chartjs needs ctx
+
+// collect all data
+
+// creates a data object that gets passed all our other arrays, based off of the example from chartjs
+
+//calls a new Chart and pass in ctx and our data
+};
